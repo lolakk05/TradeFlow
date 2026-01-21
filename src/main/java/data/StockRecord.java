592e6ac36f0ba0e85d8c2e66eea67d52c141c43d
lolakk05 <k.stolar2005@gmail.com>
2026-01-21 08:33:01 +0100

    feat: proccessing json

---
+++ src/main/java/data/StockRecord.java
@@ -0,0 +1,79 @@
+package data;
+
+public class StockRecord {
+    private String symbol;
+    private String date;
+    private double open;
+    private double high;
+    private double low;
+    private double close;
+
+    StockRecord(String symbol, String date, double open, double high, double low, double close) {
+        this.symbol = symbol;
+        this.date = date;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public void setClose(double close) {
+        this.close = close;
+    }
+
+    @Override
+    public String toString() {
+        return "StockRecord{" +
+                "symbol='" + symbol + '\'' +
+                ", date='" + date + '\'' +
+                ", open=" + open +
+                ", high=" + high +
+                ", low=" + low +
+                ", close=" + close +
+                '}';
+    }
+}
